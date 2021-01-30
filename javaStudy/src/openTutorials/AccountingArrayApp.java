8ea34d3cfc8974a44be8685e6449e78fba3a5a1a
yck <yck@DESKTOP-HFGT7NO>
2021-01-30 17:18:53 +0900

    21-01-30 생활코딩 JAVA 1

---
+++ javaStudy/src/openTutorials/AccountingArrayApp.java
@@ -0,0 +1,41 @@
+package openTutorials;
+
+public class AccountingArrayApp {
+
+	public static void main(String[] args) {
+		// 생활코딩 JAVA1 나의 앱 만들기 2
+		
+		double valueOfSupply = Double.parseDouble(args[0]);
+		double vatRate = 0.1;
+		double expenseRate = 0.3;
+		double vat = valueOfSupply*vatRate;
+		double total = valueOfSupply + vat;
+		double expense = valueOfSupply*expenseRate;
+		double income = valueOfSupply - expense;
+		
+		// 배열 : 연관된 데이터를 정리정돈하는 수단.
+		double[] dividendRates = new double[3];
+		dividendRates[0] = 0.5;
+		dividendRates[1] = 0.3;
+		dividendRates[2] = 0.2;
+		
+		double dividend1 = income * dividendRates[0];
+		double dividend2 = income * dividendRates[1];
+		double dividend3 = income * dividendRates[2];
+		
+		System.out.println("Value of supply : " + valueOfSupply);
+		System.out.println("VAT : " + vat);
+		System.out.println("Total : " + total);
+		System.out.println("Expense : " + expense );
+		System.out.println("Income : " + income);
+		System.out.println("Dividend 1 : " + dividend1);
+		System.out.println("Dividend 2 : " + dividend2);
+		System.out.println("Dividend 3 : " + dividend3);
+		
+		
+		
+		
+
+	}
+
+}
---
+++ javaStudy/src/openTutorials/AccountingArrayLoopApp.java
@@ -0,0 +1,39 @@
+package openTutorials;
+
+public class AccountingArrayLoopApp {
+
+	public static void main(String[] args) {
+		// 생활코딩 JAVA1 나의 앱 만들기 2 - 강의8
+		
+		double valueOfSupply = Double.parseDouble(args[0]);
+		double vatRate = 0.1;
+		double expenseRate = 0.3;
+		double vat = valueOfSupply*vatRate;
+		double total = valueOfSupply + vat;
+		double expense = valueOfSupply*expenseRate;
+		double income = valueOfSupply - expense;
+		
+		
+		System.out.println("Value of supply : " + valueOfSupply);
+		System.out.println("VAT : " + vat);
+		System.out.println("Total : " + total);
+		System.out.println("Expense : " + expense );
+		System.out.println("Income : " + income);
+		
+		double[] dividendRates = new double[3];
+		dividendRates[0] = 0.5;
+		dividendRates[1] = 0.3;
+		dividendRates[2] = 0.2;
+		
+		int i = 0;
+		while(i < dividendRates.length) {	//dividendRates.length : 3
+			System.out.println("Dividend : " + (income * dividendRates[i]));
+			i = i + 1;
+		}
+		
+		
+		
+
+	}
+
+}
