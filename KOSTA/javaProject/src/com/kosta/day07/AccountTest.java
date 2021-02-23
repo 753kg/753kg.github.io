361392e694b36af49a9978de5f95ac81bb49eba7
753kg <76416337+753kg@users.noreply.github.com>
2021-02-23 17:50:55 +0900

    상속, 인터페이스

---
+++ KOSTA/javaProject/src/com/kosta/day07/AccountTest.java
@@ -0,0 +1,74 @@
+package com.kosta.day07;
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+//		method1();
+//		method2();
+//		method3();
+//		method4();
+		method5();
+
+	}
+	
+	private static void method5() {
+		BonusAccount acc = new BonusAccount("555", "lee", 10000, 50);
+		acc.deposit(2000);
+		acc.deposit(2000);
+		System.out.println(acc.bonusPoint + "포인트");
+		System.out.println(acc);
+	}
+
+	private static void method4() {
+		CreditLineAccount acc = 
+				new CreditLineAccount("112-1555",
+						"홍길동",1000, 500);
+		acc.deposit(2000);
+		System.out.println(acc.withdraw(3500) + "출금");
+		System.out.println(acc);
+		
+	}
+
+	private static void method3() {
+		// CheckingAccount를 new하면
+		// Object 생성 -> Account 생성 -> CheckingAccount 생성됨
+		CheckingAccount acc = new CheckingAccount("111", "kim", 1000, "5555");
+		
+		System.out.println(acc instanceof Object);
+		System.out.println(acc instanceof Account);
+		System.out.println(acc instanceof CheckingAccount);
+		
+		System.out.println(acc);
+		acc.deposit(2000);
+		System.out.println(acc.withdraw(500));
+		System.out.println(acc.pay("5555", 200));
+		System.out.println(acc);
+	}
+	
+	private static void method2() {
+		CheckingAccount acc = new CheckingAccount();
+		acc.setAccNo("111");
+		acc.setOwnerName("kim");
+		acc.setBalance(1000);
+		//acc.setCardNo("5555");
+		System.out.println(acc);
+		
+		acc.deposit(2000);
+		System.out.println(acc.withdraw(500));
+		System.out.println(acc.pay("5555", 200));
+		System.out.println(acc);
+	}
+
+	private static void method1() {
+		Account acc = new Account();
+		acc.setAccNo("111");
+		acc.setOwnerName("kim");
+		acc.setBalance(1000);
+		System.out.println(acc);
+		
+		acc.deposit(2000);
+		System.out.println(acc.withdraw(500));
+		System.out.println(acc);
+	}
+
+}
